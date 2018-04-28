f3e822e4651c194c85efa3c6fae6c57ff4989097
Pablo Rocha Moreira <prm.gredes@gmail.com>
2018-04-28 17:24:10 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -9,6 +9,10 @@ def dictionary(word)
         "two" => '2',
         "too" => '2',
         "for"  => '4',
-        "four" => '4'
+        "four" => '4',
+        "be" => 'b',
+        "you" => 'u',
+        "at" => '@',
+        "and" => '&'
     }
 end
